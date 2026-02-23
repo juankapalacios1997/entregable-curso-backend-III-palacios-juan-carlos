1abaf6fd9522bcf75373f13caaeffb37748001fb
juankapalacios1997 <jcmaster_1997@hotmail.com>
2026-02-22 21:42:39 -0600

    Encrypting

--- src/models/mockusers.model.js
+++ src/models/mockusers.model.js
@@ -1,9 +1,22 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-export const mockUsersModel = mongoose.model('mockusers', new mongoose.Schema({
+const mockUsersSchema = mongoose.Schema({
     name: String,
     email: String,
     password: String,
     role: String,
     pets: [],
-}));
+});
+
+mockUsersSchema.pre("save", async function () {
+    if (!this.isModified("password")) return next();
+
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+});
+
+export const mockUsersModel = mongoose.model(
+    "MockUsers",
+    mockUsersSchema
+);
